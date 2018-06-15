55b83c279ab244e4e9ab2e4e4bcd004d3bd81567
karlp-asrs <karlp@azasrs.gov>
2018-06-15 15:40:02 -0700

    add fv.tr function

--- asrsMethods/R/npv_functionsss.R
+++ asrsMethods/R/npv_functionsss.R
@@ -91,3 +91,51 @@ npv.z=function(i,cf,freq=1,apr=FALSE,now=NULL,drop.bef.now=TRUE,adjdisc=NULL) {
   d=(1+i)^ind
   sum(cf/d)
 }
+
+#' FV with time varying interest rates
+#' 
+#' calculates FV from cash flow and a total return index
+#' @param CF is the cash flow zoo object
+#' @param TR is the total return time series
+#' @param ValDate is the valuation date defaults to last date in time series
+#' @keywords present_value
+#' @export
+#' @examples 
+#' cf=zoo(c(1,2),as.Date("2018-1-1")+c(0,30))
+#' tr=zoo(c(101,102),as.Date("2018-1-1)+c(0,30))
+#' fv.tr(cf,tr)
+fv.tr=function(CF,TR,ValDate=NULL) {  
+  #
+  # DESCRIPTION:
+  # a function that calculates future value with time varying interest rates
+  # 
+  # ARGUMENTS:
+  # CF -- a zoo object of cash flows
+  # TR -- a zoo object which is a total return index reflecting the variable interest rate, 
+  #       there must be an entry in TR for every date in CF and for the ValDate
+  # ValDate -- the valuation date or dates, defaults to last date in cash flow if none provided,
+  #             if earlier than last date in cash flows, then cash flows after this date are ignored,
+  #             if earlier than first date in cash flows, returns NA
+  #             defaults to the last date in the cash flow,
+  #             must be in the same date format as CF and TR
+  #
+  # RESULT:
+  # a zoo object of the same length as ValDate which is the value of the cash flows as if invested in the index
+  # as of each valuation date
+  #
+  if(is.null(ValDate)) ValDate=tail(time(CF),1)
+  if(!is.zoo(CF)) stop("CF not a zoo object")
+  if(!is.zoo(TR)) stop("TR not a zoo object")
+  if(any(!(ValDate%in%time(TR)))) stop("No TR for ValDate")
+  if(any(!(time(CF)%in%time(TR)))) stop("No TR for CF")
+  value=vector()
+  for (i in 1:length(ValDate)) {
+    if(ValDate[i]<min(time(CF))) value[i]=NA
+    CF.i=CF[time(CF)<=ValDate[i]]
+    TR.i=TR[time(TR)<=ValDate[i]]
+    TR.ind=(as.numeric(tail(TR.i,1)))/TR.i
+    value[i]=-sum(TR.ind[time(CF.i)]*CF.i)
+  }
+  return(zoo(value,ValDate))
+}
+
